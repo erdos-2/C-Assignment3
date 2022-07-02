0585fcff4dda4208d25efccf09a130d2fa024d83
Iyob G <e.yob.9h@gmail.com>
2022-07-02 19:29:36 +0300

    checked everything works right

--- Program.cs
+++ Program.cs
@@ -11,8 +11,13 @@ namespace CSharpAssignment3
     class Circle : Shape
     {
         public double Radius { get; set; }
+        public Circle() { }
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
         static readonly double PI = 3.14;
-        override public double calculateArea()
+        public override double calculateArea()
         {
             return PI * Radius * Radius;
         }
@@ -26,6 +31,12 @@ namespace CSharpAssignment3
     {
         public double Length { get; set; }
         public double Width { get; set; }
+        public Rectangle() { }
+        public Rectangle(double length, double width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
         public override double calculateArea()
         {
             return Length * Width;
@@ -46,12 +57,14 @@ namespace CSharpAssignment3
             Console.WriteLine("Area: " + circle1.calculateArea());
             Console.WriteLine("Perimeter: " + circle1.calculatePerimeter());
 
+            Console.WriteLine();
+
             Rectangle rectangle1 = new Rectangle();
             Console.WriteLine("Rectangle");
             Console.Write("Length: ");
-            double length = double.Parse(Console.ReadLine());
+            rectangle1.Length = Convert.ToDouble(Console.ReadLine());
             Console.Write("Width: ");
-            double width = double.Parse(Console.ReadLine());
+            rectangle1.Width = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Area: " + rectangle1.calculateArea());
             Console.WriteLine("Perimeter: " + rectangle1.calculatePerimeter());
         }
